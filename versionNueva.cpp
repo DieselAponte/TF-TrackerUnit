2b1992827495785ac0dcbef1e0409f93832b4685
Jose Fabian Escajadillo <jose.escajadillos@usil.pe>
2024-06-15 22:28:55 -0500

    Añadir nuevo archivo y cambios

--- versionNueva.cpp
+++ versionNueva.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <unordered_map>
 #include <string>
+//FALTA AGREGAR LOS RESTRINGIR DATOS 
+//FALTA AGREGAR LA CLASE TAREAS
+//FALTA OPERACIONES DE CONFIGURACION DE USUARIO
+// HABRÁ VERIFICACION DE CORREO (es decir que tenga si o si @)? 
 
 using namespace std;
 
@@ -16,6 +20,7 @@ class Usuario{
         int getEdad();
         string getNombre();
         string getCorreo();
+        string getContrasena();
         
 };
 
@@ -35,6 +40,10 @@ string Usuario::getNombre(){
 
 string Usuario::getCorreo(){
     return correo;
+}
+
+string Usuario::getContrasena(){
+    return contrasena;
 }
 
 class Programa {
@@ -57,27 +66,85 @@ public:
     }
 };
 
+void abrirApp(Programa& programa);
+void menu(Programa& programa, bool& salirApp,Usuario*& actual);
 void crearUsuario(Programa& programa);
 bool inicio(Programa& programa);
+Usuario* logging(Programa& programa);
 
 int main(){
     Programa programa;
+    abrirApp(programa);
+}   
+
+void abrirApp(Programa& programa){
+    bool salircuenta=false,salirApp=false;
     bool registroInicio;
+    int respuestaSalida;
+    Usuario* actual=nullptr;
     do{
+        do{
         registroInicio=inicio(programa);
-    }while(registroInicio==false);
-    cout<<" hOLA MUNDO";
+        }while(registroInicio==false);
+        actual=logging(programa);
+        if(actual!=nullptr){
+            cout<<"\n Bienvenido de nuevo "<<actual->getNombre()<<"!!!"<<endl;
+            do{
+                menu(programa,salircuenta,actual);
+            }while(salircuenta==false);
+            cout<<"\n DESEA SALIR DE LA APP??"<<endl;
+            cout<<" 1. SI"<<endl;
+            cout<<" 2. NO"  <<endl;
+            cin>>respuestaSalida;
+        }
+        
+        if(respuestaSalida==1){
+            salirApp=true;
+        }
+    }while(salirApp==false);
 }
+    
+void menu(Programa& programa, bool& salircuenta, Usuario*& actual ){
+    int opcion;
+    string menu= "\n///////Menú://////\n"
+                 "1. Ver tareas pendientes\n"
+                 "2. Ver usuario\n"
+                 "3. Salir\n"
+                 "Seleccione una opcion: ";
+    cout<<menu;
+    cin>>opcion;
+    switch (opcion){
+        case 1: 
+            // Implementar ver tareas pendientes
+            cout<<"h ola";
+            break;
+        case 2: 
+            cout<<"\n Nombre de usuario: "<<actual->getNombre()<<endl;
+            cout<<"\n Correo: "<<actual->getCorreo()<<endl;
+            cout<<"\n edad: "<<actual->getEdad()<<endl;
+            break;
+        case 3: 
+            salircuenta=true;
+            break;
+        default:
+            cout << "Opcion inválida. Inténtelo de nuevo.\n";
+            break;
+    }
+    if (salircuenta) {
+    return;
+    }
+}
+
+
 
 bool inicio(Programa& programa){
     int respuesta;
-    cout<<"Bienvenido Elija una opcion: 1 o 2: ";
+    cout<<"Bienvenido elija una opcion: 1 (CREAR USUARIO) o 2 (ACCEDER A USUARIO): ";
     cin>>respuesta;
     if(respuesta==1){
         crearUsuario(programa);
         return false;
     }else{
-        cout<<"\n Elija usuario ya existente";
         return true;
     }
 }
@@ -100,6 +167,27 @@ void crearUsuario(Programa& programa){
         programa.agregarUsuario(nuevo);
         cout<<"\n CREADO"<<endl;
     }else{
-        cout<<"\n Ya existe un usuario vinculado con ese correo";
+        cout<<"\n Ya existe un usuario vinculado con ese correo"<<endl;
+    }
+}
+
+Usuario* logging(Programa& programa){
+    string correo, contrasena;
+    cout<<"\n Ingrese su correo:  ";
+    cin>>correo;
+    Usuario* usuario=programa.recorreListaUsuarios(correo);
+    if(usuario==nullptr){
+        cout<<"\n No existe ninguna cuenta ligada al correo proporcionado"<<endl;
+        return nullptr;
+    }else{
+        cout<<"\n Ingrese su contrasena: "<<endl;
+        cin>>contrasena;
+        if(usuario->getContrasena()==contrasena){
+            cout<<"\n Sesion iniciada \n";
+            return usuario;
+        }else{
+            cout<<"\n Contraseña incorrecta \n"<<endl;
+            return nullptr;
+        }
     }
 }
