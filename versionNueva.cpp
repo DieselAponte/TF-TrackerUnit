8184fcc5511ad16889dbe80a9251d9dabde91977
Jose Fabian Escajadillo <jose.escajadillos@usil.pe>
2024-06-15 19:14:39 -0500

    Añadir nuevo archivo y cambios

--- versionNueva.cpp
+++ versionNueva.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
+#include <unordered_map>
+#include <string>
+
 using namespace std;
 
 class Usuario{
     private:
-    string nombre;
-    int edad;
-    string contrasena;
-    string correo;
+        string nombre;
+        int edad;
+        string contrasena;
+        string correo;
 
     public:
-    Usuario(string nombre,int edad,string correo, string contrasena);
-    int getEdad();
-    string getNombre();
-    string getCorreo();
-    string getContrasena();
+        Usuario(string nombre,int edad,string correo, string contrasena);
+        int getEdad();
+        string getNombre();
+        string getCorreo();
+        
 };
 
 Usuario::Usuario(string nombre,int edad,string correo, string contrasena){
@@ -33,27 +36,45 @@ string Usuario::getNombre(){
 string Usuario::getCorreo(){
     return correo;
 }
-string Usuario::getContrasena(){
-    return contrasena;
-}
 
-void crearUsuario();
-Usuario* recorreListaUsuarios(string correo);
-bool inicio();
+class Programa {
+private:
+    unordered_map<string, Usuario*> usuarios; // Hashtable de usuarios
+
+public:
+    Programa() {}
+
+    void agregarUsuario(Usuario* usuario) {
+        usuarios[usuario->getCorreo()] = usuario;
+    }
+
+    Usuario* recorreListaUsuarios(string correo) {
+        auto it = usuarios.find(correo);
+        if (it!= usuarios.end()) {
+            return it->second;
+        }
+        return nullptr;
+    }
+};
+
+void crearUsuario(Programa& programa);
+bool inicio(Programa& programa);
+
 int main(){
+    Programa programa;
     bool registroInicio;
-    //do{
-        //registroInicio=inicio();
-    //}while(registroInicio==false);
+    do{
+        registroInicio=inicio(programa);
+    }while(registroInicio==false);
     cout<<" hOLA MUNDO";
 }
 
-bool inicio(){
+bool inicio(Programa& programa){
     int respuesta;
-    cout<<"Bienvenido Elija una opcion: 1 o 2";
+    cout<<"Bienvenido Elija una opcion: 1 o 2: ";
     cin>>respuesta;
     if(respuesta==1){
-        crearUsuario();
+        crearUsuario(programa);
         return false;
     }else{
         cout<<"\n Elija usuario ya existente";
@@ -61,7 +82,7 @@ bool inicio(){
     }
 }
 
-void crearUsuario(){
+void crearUsuario(Programa& programa){
     string nomUsuario,contrasena,correo;
     int edad;
     cout<<" CREA UNA NUEVA CUENTA: ";
@@ -69,15 +90,15 @@ void crearUsuario(){
     cin>>nomUsuario;
     cout<<"\n Ingrese su correo:  ";
     cin>>correo;
-    Usuario* usuario=recorreListaUsuarios(correo);
+    Usuario* usuario=programa.recorreListaUsuarios(correo);
     if(usuario==nullptr){
         cout<<"\n Elija una contrasena: "<<endl;
         cin>>contrasena;
         cout<<"\n Ingrese su edad: "<<endl;
         cin>>edad;
-        Usuario nuevo(nomUsuario,edad,correo,contrasena);
-        //agregar a hashtable o lista
-        cout<<"\n CREADO";
+        Usuario* nuevo = new Usuario(nomUsuario,edad,correo,contrasena);
+        programa.agregarUsuario(nuevo);
+        cout<<"\n CREADO"<<endl;
     }else{
         cout<<"\n Ya existe un usuario vinculado con ese correo";
     }
